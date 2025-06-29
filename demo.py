142dd94df1907fd8ed21d9d51e18ea50e1dc6348
LeeKahYin <kyin1501@gmail.com>
2025-06-29 18:28:26 +0800

    exception added

--- demo.py
+++ demo.py
@@ -6,3 +6,14 @@ logging.info("This is an info message.")
 logging.warning("This is a warning message.")
 logging.error("This is an error message.")
 logging.critical("This is a critical message.")
+
+# below code is to check the exception config
+from src.logger import logging
+from src.exception import myException
+import sys
+
+try:
+    a = 1+'Z'
+except Exception as e:
+    logging.info(e)
+    raise myException(e, sys) from e
